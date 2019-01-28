f0e7f53615d9cb1d907da0a5a1652c139fb14aa0
BlazesRus <BlazesRus@yahoo.com>
2019-01-28 01:23:25 -0500

    successful build

---
+++ Build/source/blender/gpu/shaders/gpu_shader_flat_color_alpha_test_0_frag.glsl.c
@@ -0,0 +1,10 @@
+/* DataToC output of file <gpu_shader_flat_color_alpha_test_0_frag_glsl> */
+
+extern int datatoc_gpu_shader_flat_color_alpha_test_0_frag_glsl_size;
+extern char datatoc_gpu_shader_flat_color_alpha_test_0_frag_glsl[];
+
+int datatoc_gpu_shader_flat_color_alpha_test_0_frag_glsl_size = 142;
+char datatoc_gpu_shader_flat_color_alpha_test_0_frag_glsl[] = {
+ 13, 10,102,108, 97,116, 32,105,110, 32,118,101, 99, 52, 32,102,105,110, 97,108, 67,111,108,111,114, 59, 13, 10,111,117,116, 32,118,101, 99, 52, 32,102,114, 97,103, 67,111,108,111,114, 59, 13, 10, 13, 10,118,111,105,100, 32,109, 97,105,110, 40, 41, 13, 10,123, 13, 10,  9,105,102, 32, 40,102,105,110, 97,108, 67,111,108,111,114, 46, 97, 32, 62, 32, 48, 46, 48, 41, 13, 10,  9,  9,102,114, 97,103, 67,111,108,111,114, 32, 61, 32,102,105,110, 97,108, 67,111,108,111,114, 59, 13, 10,  9,101,108,115,101, 13, 10,  9,  9,100,105,115, 99, 97,114,100, 59, 13, 10,125, 13, 10,0
+};
+
---
+++ Build/source/blender/gpu/shaders/gpu_shader_flat_id_frag.glsl.c
@@ -0,0 +1,10 @@
+/* DataToC output of file <gpu_shader_flat_id_frag_glsl> */
+
+extern int datatoc_gpu_shader_flat_id_frag_glsl_size;
+extern char datatoc_gpu_shader_flat_id_frag_glsl[];
+
+int datatoc_gpu_shader_flat_id_frag_glsl_size = 84;
+char datatoc_gpu_shader_flat_id_frag_glsl[] = {
+ 13, 10,102,108, 97,116, 32,105,110, 32,117,105,110,116, 32,102,105,110, 97,108, 73,100, 59, 13, 10,111,117,116, 32,117,105,110,116, 32,102,114, 97,103, 73,100, 59, 13, 10, 13, 10,118,111,105,100, 32,109, 97,105,110, 40, 41, 13, 10,123, 13, 10,  9,102,114, 97,103, 73,100, 32, 61, 32,102,105,110, 97,108, 73,100, 59, 13, 10,125, 13, 10,0
+};
+
